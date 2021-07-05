1ec87b65ef9e08d9fc722dd172df6c36e4eac8a3
DyningAida <aida.batrishya@gmail.com>
2021-07-05 09:47:05 +0700

    update

--- 5_Sistem_Pakar_Mental_Illness_Diagnose_dengan_NB/main.py
+++ 5_Sistem_Pakar_Mental_Illness_Diagnose_dengan_NB/main.py
@@ -19,6 +19,17 @@ tabel_kondisi = ['tinggal_dengan_ortu','pernah_gapyear','lama_gapyear',
 @app.route("/",methods=['POST','GET'])
 def index():
     if request.method == "POST":
+        data_kondisii.clear()
+        data_diri.clear()
+        username = request.form['username']
+        if len(username) == 0:
+            return render_template('form_awal.html')
+        return render_template('form.html',username=username)
+    return render_template('form_awal.html')
+
+@app.route("/form",methods=['POST','GET'])
+def form():
+    if request.method == "POST":
         for i in tabel:
             data_diri.append(int(request.form[i]))
         return render_template('data_diri.html',data=data_diri)
@@ -34,6 +45,7 @@ def save_profile():
 @app.route("/data_kondisi",methods=['POST','GET'])
 def data_kondisi():
     if request.method == "POST":
+        data_kondisii.clear()
         for a in tabel_kondisi:
             data_kondisii.append(int(request.form[a]))
         print(data_kondisii)
@@ -43,61 +55,37 @@ def data_kondisi():
 @app.route("/save_kondisi",methods=['POST','GET'])
 def save_kondisi():
     if request.method == "POST":
-        # analisis juga dipanggil disini
-        df = pd.read_csv("mental_illness.csv", sep=',', encoding='latin1')
-        # encode atribut
-        encode = LabelEncoder()
-        df['pendidikan'] = encode.fit_transform(df['pendidikan'])
-        df['usia'] = encode.fit_transform(df['usia'])
-        df['gender'] = encode.fit_transform(df['gender'])
-        # shuffle data
-        df = df.sample(frac=1)
-        # independent variable
-        x = df.drop(["mental_illness","pendidikan","memiliki_komputer","akses_internet_memadai",
-                    "tinggal_di_rumah_subsidi","gender","kerja_parttime"
-                    ,"bekerja_dan_sekolah","menerima_bantuan_sosial"], axis = 1)
-        x.head()
-        # dependent variable
-        y = df["mental_illness"]
-        y.head()
-        # split data train dan data test
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state = 123)
-        # memanggil naive bayes classifier
-        nb = GaussianNB()
-        # menginputkan data training
-        nb_train = nb.fit(x_train, y_train)
-        # Menentukan hasil prediksi dari x_test
-        y_pred = nb_train.predict([data_kondisii])
-        print(y_pred)
+        if len(data_kondisii) == 15:
+            # analisis juga dipanggil disini
+            df = pd.read_csv("mental_illness.csv", sep=',', encoding='latin1')
+            # encode atribut
+            encode = LabelEncoder()
+            df['pendidikan'] = encode.fit_transform(df['pendidikan'])
+            df['usia'] = encode.fit_transform(df['usia'])
+            df['gender'] = encode.fit_transform(df['gender'])
+            # shuffle data
+            df = df.sample(frac=1)
+            # independent variable
+            x = df.drop(["mental_illness","pendidikan","memiliki_komputer","akses_internet_memadai",
+                        "tinggal_di_rumah_subsidi","gender","kerja_parttime"
+                        ,"bekerja_dan_sekolah","menerima_bantuan_sosial"], axis = 1)
+            x.head()
+            # dependent variable
+            y = df["mental_illness"]
+            y.head()
+            # split data train dan data test
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state = 123)
+            # memanggil naive bayes classifier
+            nb = GaussianNB()
+            # menginputkan data training
+            nb_train = nb.fit(x_train, y_train)
+            # Menentukan hasil prediksi dari x_test
+            y_pred = nb_train.predict([data_kondisii])
+            print(y_pred)
+        else:
+            return redirect(url_for('index'))
         return render_template('analysis_result.html', y_pred=y_pred)
     return render_template('result.html')
-
-def diagnose(x_test):
-    df = pd.read_csv("mental_illness.csv", sep=',')
-    # encode atribut
-    encode = LabelEncoder()
-    df['pendidikan'] = encode.fit_transform(df['pendidikan'])
-    df['usia'] = encode.fit_transform(df['usia'])
-    df['gender'] = encode.fit_transform(df['gender'])
-    # shuffle data
-    df = df.sample(frac=1)
-    # independent variable
-    x = df.drop(["mental_illness","pendidikan","memiliki_komputer","akses_internet_memadai",
-                "tinggal_di_rumah_subsidi","gender","kerja_parttime"
-                ,"bekerja_dan_sekolah","menerima_bantuan_sosial"], axis = 1)
-    x.head()
-    # dependent variable
-    y = df["mental_illness"]
-    y.head()
-    # split data train dan data test
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state = 123)
-    # memanggil naive bayes classifier
-    nb = GaussianNB()
-    # menginputkan data training
-    nb_train = nb.fit(x_train, y_train)
-    # Menentukan hasil prediksi dari x_test
-    y_pred = nb_train.predict(x_test)
-    return y_pred
 
 if __name__ == '__main__':
     app.run(debug=True)
